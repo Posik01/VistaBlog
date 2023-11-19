733cce71f636da1ee971544d6f667d6d278f644c
Max Posikera <sokbro48@gmail.com>
2023-11-19 16:00:27 +0200

    Add project files.

---
+++ VistaBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VistaBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
