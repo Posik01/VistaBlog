9fddd088bfadc7a6c6e90cef81abc4ed076b1fb8
Max Posikera <sokbro48@gmail.com>
2023-11-28 22:42:15 +0200

    add: reset password func

--- VistaBlog/ViewModels/UserVM.cs
+++ VistaBlog/ViewModels/UserVM.cs
@@ -6,6 +6,8 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? UserName { get; set; }
-        
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+
     }
 }
