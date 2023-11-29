191cbf873d39d1de44bd9022fbcc21c99f8bde46
Max Posikera <sokbro48@gmail.com>
2023-11-29 18:44:50 +0200

    add: create post view + migration

--- VistaBlog/Areas/Admin/Controllers/PostController.cs
+++ VistaBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using VistaBlog.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace VistaBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class PostController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVM());
         }
     }
 }
