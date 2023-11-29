4a64e6698e73670c22f3c03c865de0e848597315
Max Posikera <sokbro48@gmail.com>
2023-11-29 15:40:22 +0200

    add: access denied func

---
+++ VistaBlog/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VistaBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- VistaBlog/Areas/Admin/Controllers/UserController.cs
+++ VistaBlog/Areas/Admin/Controllers/UserController.cs
@@ -150,7 +150,7 @@ namespace VistaBlog.Areas.Admin.Controllers
             {
                 return View(new LoginVM());
             }
-            return RedirectToAction("Index", "User", new { area = "Admin" });
+            return RedirectToAction("Index", "Post", new { area = "Admin" });
         }
 
         [HttpPost("Login")]
@@ -171,15 +171,23 @@ namespace VistaBlog.Areas.Admin.Controllers
             }
             await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
             _notification.Success("Login Successful");
-            return RedirectToAction("Index", "User", new { area = "Admin" });
+            return RedirectToAction("Index", "Post", new { area = "Admin" });
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Logout()
         {
             _signInManager.SignOutAsync();
             _notification.Success("You are logged out successfully");
             return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+        [HttpGet("AccessDenied")]
+        [Authorize]
+        public IActionResult AccessDenied()
+        {
+            return View();
         }
 
     }
