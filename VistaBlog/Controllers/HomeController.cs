f90209ee038d67ca09658d688a5871328c0033b1
Max Posikera <sokbro48@gmail.com>
2023-11-30 20:36:31 +0200

    add: home page design

--- VistaBlog/Controllers/HomeController.cs
+++ VistaBlog/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using VistaBlog.Data;
 using VistaBlog.Models;
+using VistaBlog.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
 
 namespace VistaBlog.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                                ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.Include(x => x.ApplicationUser).ToList();
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ VistaBlog/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using VistaBlog.Models;
+
+namespace VistaBlog.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
